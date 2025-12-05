9393a030be3f8463b4ea064bf640653bc03e851d
luciano.lee <luciano.lee@slc.com.tw>
2025-12-05 11:08:12 +0800

    fix: adjust ci issue

--- packages/core/eslint.config.js
+++ packages/core/eslint.config.js
@@ -1,3 +1,6 @@
+import tsParser from "@typescript-eslint/parser";
+import tsPlugin from "@typescript-eslint/eslint-plugin";
+
 export default [
   {
     ignores: ["dist/**"],
@@ -5,8 +8,14 @@ export default [
   {
     files: ["src/**/*.ts"],
     languageOptions: {
-      ecmaVersion: "latest",
-      sourceType: "module",
+      parser: tsParser,
+      parserOptions: {
+        ecmaVersion: "latest",
+        sourceType: "module",
+      },
+    },
+    plugins: {
+      "@typescript-eslint": tsPlugin,
     },
     rules: {
       "@typescript-eslint/no-unused-vars": ["warn", { argsIgnorePattern: "^_" }],
